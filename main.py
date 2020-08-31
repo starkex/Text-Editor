d26cfcba483c1fdadd1426760aefbade4227e4f9
Shubham Gupta <61947842+starkex@users.noreply.github.com>
2020-08-31 17:11:21 +0530

    imports

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import font,colorchooser,filedialog,messagebox
+import os 
+
+
+win=tk.Tk()
+win.title('Neutron Editor')
+win.geometry('1000x800')
+
+
+win.mainloop()
