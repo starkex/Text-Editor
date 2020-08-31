c7796eb781c1c4eae98992d7302c5c43b22b97e3
Shubham Gupta <61947842+starkex@users.noreply.github.com>
2020-08-31 17:39:31 +0530

    menu

--- main.py
+++ main.py
@@ -3,10 +3,23 @@ from tkinter import ttk
 from tkinter import font,colorchooser,filedialog,messagebox
 import os 
 
-
 win=tk.Tk()
+
 win.title('Neutron Editor')
 win.geometry('1000x800')
 
+main_menu = tk.Menu()
 
+file = tk.Menu(main_menu,tearoff=False)
+Edit = tk.Menu(main_menu,tearoff=False) 
+view = tk.Menu(main_menu,tearoff=False)
+color_theme = tk.Menu(main_menu,tearoff=False) 
+
+main_menu.add_cascade(label='File',menu=file)
+main_menu.add_cascade(label='Edit',menu=Edit)
+main_menu.add_cascade(label='View',menu=view)
+main_menu.add_cascade(label='Theme',menu=color_theme)
+
+
+win.config(menu=main_menu)
 win.mainloop()
