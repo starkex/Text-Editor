24e028ba54c79831f707ffe1eaeb0153480867d5
Shubham Gupta <61947842+starkex@users.noreply.github.com>
2020-10-10 21:22:22 +0530

    finalize

---
+++ setup_main.py
@@ -0,0 +1,6 @@
+from cx_Freeze import setup, Executable
+setup(name = "Neutron",
+      version="1.0",
+      description="Use this as an alternative to notepad ",
+      executables=[Executable(r"C:\Users\stark\Desktop\Text-Editor\main.py")]
+      )
